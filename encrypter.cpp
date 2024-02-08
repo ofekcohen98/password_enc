aee5ab862486df47ef6ae80b3117725ce43e721f
ofek_cohen_5 <ofek.cohen1@mail.huji.ac.il>
2024-02-08 16:45:56 +0200

    first commition

---
+++ encrypter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ofekc on 08/02/2024.
+//
+
+#include "encrypter.h"
---
+++ encrypter.h
@@ -0,0 +1,13 @@
+//
+// Created by ofekc on 08/02/2024.
+//
+
+#ifndef _ENCRYPTER_H_
+#define _ENCRYPTER_H_
+
+class encrypter
+{
+
+};
+
+#endif //_ENCRYPTER_H_
